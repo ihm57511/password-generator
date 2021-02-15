bcb76a89a0e705529c0c102b348c5fac7100c2dd
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-02-14 22:04:22 -0500

    successful iteration of generatePassword function

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
+
+
+
 // Setting dynamic range sliders
 var slider = document.getElementById("number of characters");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
+slider.oninput = function () {
   output.innerHTML = this.value;
-}
+  slider = slider.value;
+};
+
+
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+var special = ["~","!","@","#","$","%","^","&","*","(",")"];
+var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var available = letters.concat(numbers).concat(special).concat(upper);
+
+
 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+  function generatePassword() {
+    var password = []
+    for(var i=0; i < 8; i++) {
+      password.push(available[Math.floor(Math.random() * available.length)]);
+    } return 
+  }
 
 // Write password to the #password input
 function writePassword() {
@@ -23,3 +43,91 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(available)
+// function set_var(available) {
+//   document.querySelector("letters").value;
+// add event listener onchange 
+// if = 0 sclice(letters)
+// else .concat(letters)
+//   document.querySelector("numbers").value;
+//   document.querySelector("special-characters").value;
+//   document.querySelector("upper-case").value;
+
+
+// // // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+//   var num = document.getElementById("number of characters").value;
+//   // var available = setAvail();
+//   var available = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  
+
+//   function generatePassword() {
+//     var password = []
+//     for(var i=0; i < 8; i++) {
+//       password.push(available[Math.floor(Math.random() * available.length)]);
+//     } return password
+//   }
+  
+//   passwordText.value = password;
+//   // Add event listener to generate button
+//   generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+// onchange	An HTML element has been changed
+// onclick	The user clicks an HTML element
+// onmouseover	The user moves the mouse over an HTML element
+// onmouseout	The user moves the mouse away from an HTML element
+// onkeydown	The user pushes a keyboard key
+// onload	The browser has finished loading the page
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
