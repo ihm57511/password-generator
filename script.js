202a5f06f441d01786cc3c66a6edec36c844cdbb
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-02-16 16:49:34 -0500

    aall operational outside of commas present in final password

--- script.js
+++ script.js
@@ -10,17 +10,16 @@ var numberSet = 1
 var specialSet = 1
 var upperSet = 1
 
-
-
 // Display the default slider value
 output.innerHTML = slider.value;
 
-// Update the current slider value (each time you drag the slider handle)
+// Update the "number-of-charaters" slider value (each time you drag the slider handle)
 slider.oninput = function () {
   output.innerHTML = this.value;
   num = this.value;
 }
 
+// setting slider input for character type input
 var let = document.getElementById("letters");
 let.oninput = function () {
   letterSet = this.value;
@@ -38,20 +37,19 @@ upp.oninput = function () {
   upperSet = this.value;
 }
 
+// setting vaiable arrrays for available types
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var special = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var available = [];
-
-
-
+var passwordArray = [];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// sewts character type availability based on user input
 function setAvail() {
-
   function setLetters() {
     if (letterSet === 1) {
       available.unshift(letters)
@@ -76,32 +74,41 @@ function setAvail() {
     }
   }
 
-  function setArray() {
-    return Array.from(available)
-  }
-
   setLetters()
   setNumbers()
   setSpecial()
   setUpper()
-  setArray()
+  
+  // console.log(available)
+  // console.log(numberSet)
+  // console.log(letterSet)
+  // console.log(upperSet)
+  // console.log(specialSet)
 
-  console.log(available)
-  console.log(numberSet)
-  console.log(letterSet)
-  console.log(upperSet)
-  console.log(specialSet)
+  return
+}
 
-
-
+function setArray() {
+  for (var i = 0; i < num; i++) {
+    passwordArray.push(available[Math.floor(Math.random() * available.length)]);
+    // console.log(passwordArray);
+  } 
 }
 
 function generatePassword() {
   setAvail();
+  setArray();
+  var pw = [];
   var password = [];
+  
   for (var i = 0; i < num; i++) {
-    password.push(available[Math.floor(Math.random() * available.length)]);
-  } return password;
+    password.push(passwordArray[i][Math.floor(Math.random() * passwordArray[i].length)]);
+    // console.log(password);
+  }
+  
+  var pw = password.toString();
+  // console.log(pw);
+  return pw;
 }
 
 // Write password to the #password input
@@ -121,84 +128,3 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-
-
-
-
-
-
-
-// console.log(available)
-// function set_var(available) {
-//   document.querySelector("letters").value;
-// add event listener onchange 
-// if = 0 sclice(letters)
-// else .concat(letters)
-//   document.querySelector("numbers").value;
-//   document.querySelector("special-characters").value;
-//   document.querySelector("upper-case").value;
-
-
-// // // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-//   var num = document.getElementById("number of characters").value;
-//   // var available = setAvail();
-//   var available = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
-
-//   function generatePassword() {
-//     var password = []
-//     for(var i=0; i < 8; i++) {
-//       password.push(available[Math.floor(Math.random() * available.length)]);
-//     } return password
-//   }
-
-//   passwordText.value = password;
-//   // Add event listener to generate button
-//   generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-// onchange	An HTML element has been changed
-// onclick	The user clicks an HTML element
-// onmouseover	The user moves the mouse over an HTML element
-// onmouseout	The user moves the mouse away from an HTML element
-// onkeydown	The user pushes a keyboard key
-// onload	The browser has finished loading the page
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
